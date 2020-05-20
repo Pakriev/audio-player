eb8b2d760c76b52c3d99fb99179533d3480b9ebf
Aleks <alekey.pakriev@gmail.com>
2020-05-20 19:05:01 +0500

    Закончил писать аудио плеер

--- script.js
+++ script.js
@@ -46,4 +46,52 @@ playBtn.addEventListener('click', () => {
     } else {
         playSong();
     }
-})
+});
+
+function prevSong() {
+    songIndex--;
+
+    if(songIndex < 0) {
+        songIndex = songs.length -1;
+
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+function nextSong() {
+    songIndex++;
+
+    if(songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+function updateProgress(e) {
+    const { duration, currentTime} = e.target
+    const progressPercent = (currentTime / duration) * 100;
+
+    progress.style.width = `${progressPercent}%`;
+}
+
+
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong);
